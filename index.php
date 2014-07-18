53d3c70e5b07431d00d83362b0c46bdeda7ad463
Atiqe <Atiqe@VOSTRO1.ukim.local>
2014-07-18 17:07:07 +0100

    Test

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+       <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="images/style.css" type="text/css" media="screen" /> 
+    </head>
+    <body>
+        <div class="boxshadow">
+            <code> 
+            div.boxshadow {
+            width:400px;
+            height:150px;
+            padding:10px;
+            border: 1px solid;
+            background-color: #f2f2f2;
+            border-color:#990000;
+            border-radius: 10px;
+            -moz-border-radius: 10px;
+            -webkit-border-radius: 10px;
+            box-shadow: 5px 5px 5px 5px #888888;
+            -moz-box-shadow: 5px 5px 5px 5px #888888;
+            -webkit-box-shadow: 5px 5px 5px 5px #888888;
+            }
+            </code>
+        </div>
+        
+        <?php
+        // put your code here
+        ?>
+        <br> <br>
+<table>
+  <tr>
+    <th>First name</th>
+    <th>Last name</th>
+  </tr>
+  <tr>
+    <td>Peter</td>
+    <td>Griffin</td>
+  </tr>
+  <tr>
+    <td>Lois</td>
+    <td>Griffin</td>
+  </tr>
+</table>
+        
+    </body>
+</html>
